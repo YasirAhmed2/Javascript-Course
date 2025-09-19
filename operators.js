6d941c30ae5c84de5e04c396aa2a0db9a017abe7
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-09-18 21:53:19 -0700

    operators in javascript

---
+++ operators.js
@@ -0,0 +1,28 @@
+// arithmetic operators
+let a=10;
+let b=3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+// comparison operators
+console.log(5=="5");
+console.log(x===y);  // strict equality
+console.log(x>y);
+console.log(x<y);
+console.log(x>=y);
+console.log(x<=y);
+console.log(x!=y);
+console.log(x!==y); // strict not equal
+
+// logical operators
+console.log(true && false);
+console.log(true || false);
+console.log(!true);
+
+// assignment operators
+let x=10;
+x+=5;
+console.log(x);
