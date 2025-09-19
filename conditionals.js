8a23e71cb559290c5ecee95b2d3c2ecefdf9f788
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-09-18 23:12:30 -0700

    conditionals

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+// if-else
+let score=6;
+if(score>=5){
+    console.log("You passed!");
+}else if (score===4){
+    console.log("You are on the border.");
+}else if(score<4){
+    console.log("too much less score.");
+}else{
+    console.log("You failed.");
+}
