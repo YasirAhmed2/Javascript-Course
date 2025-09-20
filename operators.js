bda5e4596c364f25d92e426e96fcdcea16f6c845
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-09-19 18:12:10 -0700

    added comments of expected output

--- operators.js
+++ operators.js
@@ -1,28 +1,33 @@
 // arithmetic operators
 let a=10;
 let b=3;
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
+console.log(a+b);  // Output: 13
+console.log(a-b); // Output: 7
+console.log(a*b); // Output: 30
+console.log(a/b); // Output: 3.3333333333333335
+console.log(a%b); // Output: 1
 
 // comparison operators
-console.log(5=="5");
-console.log(x===y);  // strict equality
-console.log(x>y);
-console.log(x<y);
-console.log(x>=y);
-console.log(x<=y);
-console.log(x!=y);
-console.log(x!==y); // strict not equal
+console.log(5=="5"); // loose equality // output: true
+console.log(5==="5"); // strict equality // output: false
+let x=10,y=20;
+console.log(x===y);  // strict equality // output: false
+console.log(x>y); // output: false
+console.log(x<y); // output: true
+console.log(x>=y); // output: false
+console.log(x<=y); // output: true
+console.log(x!=y); // output: true
+console.log(x!==y); // strict not equal // output: true
 
 // logical operators
-console.log(true && false);
-console.log(true || false);
-console.log(!true);
+console.log(true && false); // output: false
+console.log(true || false); // output: true
+console.log(!true); // output: false
+console.log(!false); // output: true
 
 // assignment operators
-let x=10;
-x+=5;
-console.log(x);
+let z=10;
+z+=5;
+console.log(z); // output: 15
+z-=3;
+console.log(z); // output: 12
