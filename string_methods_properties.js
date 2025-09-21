20c13050cfbda238a1f2eb02484bb7d72250ebde
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-09-21 05:17:07 -0700

    using charat() string method

---
+++ string_methods_properties.js
@@ -0,0 +1,13 @@
+// ********** String Methods and Properties in JavaScript **********
+// 1. Declaring string in javaScript
+let string1="Hello I'm learning JavaScript";
+let string2="Hello I'm learning Python";
+let string3= new String ("Hello I'm learning C++");  // Using String constructor
+console.log(string1); // Output: Hello I'm learning JavaScript
+console.log(string2); // Output: Hello I'm learning Python
+console.log(string3); // Output: Hello I'm learning C++
+
+// 2. using charat() method
+console.log(string1.charAt(4)); // Output: o
+console.log(string2.charAt(7)); // Output: I
+console.log(string3.charAt(10)); // Output: l
