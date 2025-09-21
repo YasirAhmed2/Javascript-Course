8657ee0a5a93748c99f87c431b476e8b949f9bbf
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-09-21 07:47:33 -0700

    added proper explanatory comments

--- functions_practice.js
+++ functions_practice.js
@@ -1,10 +1,13 @@
+// ******* JavaScript Functions Practice Problems *******
 // Description: This file contains practice problems related to functions in JavaScript.
+// 1. A Function to calculate sum of two numbers
 function calculateSum(a,b){
     return a+b;
 }
 calculateSum(10,20);
 console.log(calculateSum(10,20)); // Output: 30
 
+// 2. A function to calculate factorial of a number
 function calculateFactorial(n){
     if (n==1 || n==0){
         return 1;
@@ -13,3 +16,6 @@ function calculateFactorial(n){
     }
 }
 console.log("Factorial of 6 is: ", calculateFactorial(6)); // Output: 720
+
+// 3. A function to display table of a number
+function 
