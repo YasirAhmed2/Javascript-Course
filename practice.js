3be4e1e67fbee0009a7f3d1cc9c1e702a6c34d53
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-09-21 04:34:49 -0700

    counting number of vowels in string

--- practice.js
+++ practice.js
@@ -22,3 +22,23 @@ for(let i=1;i<=number1;i++){
     factorial*=i;
 }
 console.log("Factorial of ",number1,"is",factorial); // Output: Factorial of 5 is 120
+
+
+// Task # 04: Counting number of vowels in a string
+let str="Hello I'm learning JavaScript";
+let count=0;
+for(let i=0;i<str.length;i++){
+    if(str[i]=='a'||
+        str[i]=='e'||
+        str[i]=='i'||
+        str[i]=='o'||
+        str[i]=='u'||
+        str[i]=='A'||
+        str[i]=='E'||
+        str[i]=='I'||
+        str[i]=='O'||
+        str[i]=='U'){
+            count++;
+        } 
+}
+console.log("Number of vowels in the string is:",count); // Output: Number of vowels in the string is: 10
