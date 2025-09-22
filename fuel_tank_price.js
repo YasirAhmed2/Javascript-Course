6608991fee7c0812f4d9c18283a3f81bb6f472ad
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-09-22 09:33:58 -0700

    Fuel Tank Price calculation problem solution

--- fuel_tank_price.js
+++ fuel_tank_price.js
@@ -1,3 +1,15 @@
 // when a car fuel storage capacity current level of fuel and price per litre .
 // return how many rupees it would take to fill the tank completely.
 
+function fuelTankPrice(tankCapacity, CurrentFuel, pricePerLitre){
+    let fuelRequired=tankCapacity-CurrentFuel;
+    return fuelRequired*pricePerLitre;
+}
+
+let tankFuelCapacity=50;
+let currentFuelLevel=15;
+let perLitrePrice=200;
+
+let totalPrice=fuelTankPrice(tankFuelCapacity,currentFuelLevel,perLitrePrice);
+console.log("Total cost required to fill tank: ",totalPrice); // 7000
+
