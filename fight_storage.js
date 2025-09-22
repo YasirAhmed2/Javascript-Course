0b487d057765f094baa9efcd6d3beb0b326ee60e
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-09-22 09:27:17 -0700

    fightStorage problem final solution

--- fight_storage.js
+++ fight_storage.js
@@ -7,61 +7,39 @@ return the number of whole size files drive can put.
 */
 
 
-// function fightStorage(fileSize,fileUnit ,driveSize){
-//     if (fileUnit=='B'){
-//         driveSize=1000000000;
+function fightStorage(fileSize,fileUnit ,driveSize){
+    // let fileSizeGB;
+    let fileSizeBytes;
+    if (fileUnit=='B'){
+        // fileSizeGB=fileSize/(1000^3);
+        fileSizeBytes=fileSize;
 
-//     return (fileSize/driveSize)*10000000000;
-//     }else if(fileUnit=='KB'){
-//         driveSize=10485786;
-//         return (fileSize/driveSize)*driveSize;
-//     }else if (fileUnit=='MB') {
-//         // fileSize= fileSize;
-//         let FileSizeGb= fileSize/1000000;;
+    }else if(fileUnit=='KB'){
+        fileSizeBytes=fileSize*1000;
+        // fileSizeGB=fileSize/(1000^2);
+      
+    }else if (fileUnit=='MB') {
+        
+        // fileSizeGB= fileSize/1000;
+fileSizeBytes=fileSize*1000*1000;
+        // driveSize=1000;
+        // console.log(fileSizeGB);
+        // console.log(fileSizeGb/driveSize);    
+        //  return fileSizeBytes/driveSize;
 
-//         // driveSize=1000;
-//         console.log(FileSizeGb);
-//         console.log(FileSizeGb/driveSize);    
-//         return (fileSize/driveSize)*10000;
-//     }
-// }
+    }
+let driveSizeBytes=driveSize*1000*1000*1000;
+return driveSizeBytes/fileSizeBytes;
 
-// let's try another approach
-// 1. check file unit and convert it to bytes 
-// 2. convert drive size to bytes
-// 3. dividing drive size by file size
-function fileCaclculation(fileSize,FileUnit, driveSize){
-    let FileSizeB=0;
-if(FileUnit=='B'){
-    FileSizeB=fileSize;
-    // console.log(FileSizeB); 
-}else if(FileUnit=='KB'){
-    FileSizeB=fileSize*1000;
-    // console.log(FileSizeB);
-}else if(FileUnit=='MB'){
-    FileSizeB=fileSize*1000*1000;
-    console.log(FileSizeB);
-}
-let DriveSizeB=driveSize*1000*1000*1000;
-console.log(DriveSizeB);
-
-return DriveSizeB/FileSizeB;
-
+    // console.log(driveSize/fileSizeGB);
+    // console.log(driveSize);
+   
 }
 
 
-let fileSize=10;
-let FileUnit='KB';
-let driveSize=1;
-
-// let total_files=fightStorage(fileSize,FileUnit, driveSize);
-let total_files=fileCaclculation(fileSize,FileUnit, driveSize);
-console.log("Total files ", total_files, "of file size: ", fileSize, FileUnit, "can be stored in hard drive of size", driveSize, "GB");
+let fileSize=100;
+let FileUnit='B';
+let driveSize=4;
+console.log(fightStorage(fileSize,FileUnit,driveSize));
 
 
-
-
-
-
-// when a car fuel storage capacity current level of fuel and price per litre .
-// return how many rupees it would take to fill the tank completely.
---
+++ fuel_tank_price.js
@@ -0,0 +1,3 @@
+// when a car fuel storage capacity current level of fuel and price per litre .
+// return how many rupees it would take to fill the tank completely.
+
