164ff987a8ea564795a0eb88d8b26294ea80a8f9
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-09-22 23:15:33 -0700

    FuelPriceCalculation with user inputs and proper input validations

--- fuel_tank_price.js
+++ fuel_tank_price.js
@@ -2,14 +2,21 @@
 // return how many rupees it would take to fill the tank completely.
 
 function fuelTankPrice(tankCapacity, CurrentFuel, pricePerLitre){
-    let fuelRequired=tankCapacity-CurrentFuel;
+    if(tankCapacity<0||CurrentFuel<0||pricePerLitre<0){
+        return "Error: Invalid input values";
+    }else if (CurrentFuel==tankCapacity){
+        return "Error: Tank is already full";
+    }
+  let fuelRequired=tankCapacity-CurrentFuel;
     return fuelRequired*pricePerLitre;
-}
+    }
 
-let tankFuelCapacity=50;
-let currentFuelLevel=15;
-let perLitrePrice=200;
+
+    const prompt = require("prompt-sync")();
+let tankFuelCapacity=prompt("Enter tank fuel capacity:");
+let currentFuelLevel=prompt("Enter current fuel level:");
+let perLitrePrice=prompt("Enter price per litre:");
 
 let totalPrice=fuelTankPrice(tankFuelCapacity,currentFuelLevel,perLitrePrice);
-console.log("Total cost required to fill tank: ",totalPrice); // 7000
+console.log(totalPrice); 
 
