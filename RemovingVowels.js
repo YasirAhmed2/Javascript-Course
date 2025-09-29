7585c21834576a2e549974ddcdb965b5e48c3ba8
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-09-29 10:15:36 +0500

    Problem Statement: Function taking a string and returning back string after removing vowels from string

--- RemovingVowels.js
+++ RemovingVowels.js
@@ -1,3 +1,21 @@
 function removeVowels(str){
-    
+    let newStr='';
+    for(let i=0;i<str.length;i++){
+
+        if (str[i]!=='a'&&str[i]!=='A'
+            &&str[i]!=='e'&&str[i]!=='E'
+            &&str[i]!=='i'&&str[i]!=='I'
+            &&str[i]!=='o'&&str[i]!=='O'
+            &&str[i]!=='u'&&str[i]!=='U'
+        ){
+newStr+=str[i];
+        }
+        
+    }
+    return newStr;
+
 }
+
+let string='hello I am learning Java';
+let vowelRemoved=removeVowels(string);
+console.log("String without vowels: ", vowelRemoved);
