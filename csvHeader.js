6b4ba7c341e80e2bc93a99be3b37aa4ea81096c6
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-10-03 10:16:18 +0500

    Added 3rd october problem statements

---
+++ csvHeader.js
@@ -0,0 +1,21 @@
+/*
+    // ********* Problem Statement # 02: CSV Header *********
+Given the first line of a comma separated values (csv), return an array containing the headings.
+- The first line of csv file contains headings separated by commas.
+- Remove any leading or trailing whitespaces from each heading.
+function getHeadings(csv){
+
+}
+*/
+
+function getHeadings(csv){
+
+let csvHeadings="";
+for(let i=0;i<csv.length;i++){
+    
+}
+
+
+}
+
+let headings=getHeadings("longitude,latitude,housing_median_age,total_rooms,total_bedrooms,population,households,median_income,median_house_value,ocean_proximity");
