70aaff9e88f84495bfbd61b9446746c96ecc7ac8
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-10-10 21:11:50 +0500

    new problem with solution added

---
+++ launchFuel.js
@@ -0,0 +1,14 @@
+function launchFuel(payload) {
+  let totalFuel = 0;
+  let fuel = Math.floor(payload / 5);
+
+  while (fuel > 0) {
+    totalFuel += fuel;
+    fuel = Math.floor(fuel / 5);
+  }
+
+  return totalFuel;
+}
+
+// Example:
+console.log(launchFuel(100));  // Output: 24
